e07b9a83511bbd7ebd3a3f004764b0ce69774707
Antoine Boellinger <aboellinger@hotmail.com>
2020-05-21 11:48:05 +0200

    Rewrite script (reorganize code, make things more generic)

--- GuerillaTags/__init__.py
+++ GuerillaTags/__init__.py
@@ -1,147 +1,125 @@
-import os, os.path
-import shutil
+
+import os
+import re
 import maya.cmds as cmds
-import datetime
-import sys
 
-#-------------------------------------------- CHANGE PATH WITH OUR IMAGE -------------------------------------------------
-
-Imagepath = "P:/Groupe_02_ENNUI/03_SCRIPT/Pipeline/GuerillaTags/Icon/" 
+IMAGE_PATH = os.path.dirname(__file__) + "/Icon/"
+TAG_PATTERN = re.compile(r"\w+")
 
 
-#-------------------------------------------- TAG MANAGER -------------------------------------------------
-
-def SetTags():
-	prefix = cmds.optionMenu('prefix_tags', v=1,q=1 )
-	if prefix == 'Prefix - None':
-		prefix = ''
-	else:
-		prefix = prefix + '_'
-	tags = cmds.textFieldGrp('guerilla_tags',tx=1,q=1)
-	if tags != '':
-		tags = prefix + cmds.textFieldGrp('guerilla_tags',tx=1,q=1)
-		shapenodes = []
-		selection = cmds.ls(sl=True) # Get Selection
-
-		tags_list = tags.replace(' ', '').split(',')
-
-		for obj in selection:
-
-		    # Check if selected object is a shapeNode
-		    is_shape = cmds.objectType(obj, isType='mesh')
-
-		    if not is_shape:
-		        shapes = cmds.listRelatives(obj, shapes=True)
-		        for itm in shapes:
-		            shapenodes.append(itm)
-		    else:
-		        shapenodes.append(obj)
-
-		for shape in shapenodes:
-		    # Check if GuerillaTags attribute exists on selected
-		    has_guerilla_tags = cmds.attributeQuery('GuerillaTags', node=shape, exists=True)
-		    # If attr GuerillaTags doesnt exists, create it
-		    if not has_guerilla_tags:
-		        cmds.select(shape)
-		        cmds.addAttr( shortName='GuerillaTags', longName='GuerillaTags', dataType='string')
-		        cmds.setAttr( '{}.GuerillaTags'.format(shape),', '.join(tags_list), type='string')
-		    else:
-				print('attribute already exists')
-				existing_tags = cmds.getAttr('{}.GuerillaTags'.format(shape))
-				existing_tags_list = existing_tags.replace(' ', '').split(',')
-				final_tag_list = existing_tags_list
-				for tag in tags_list:
-					if not tag in existing_tags_list:
-						final_tag_list.append(tag)
-				cmds.setAttr( '{}.GuerillaTags'.format(shape),', '.join(final_tag_list), type='string')
-
-def DeleteTags():
-	selection = cmds.ls(sl=True) # Get Selections
-	shapenodes = []
-	for obj in selection:
-
-		# Check if selected object is a shapeNode
-		is_shape = cmds.objectType(obj, isType='mesh')
-
-		if not is_shape:
-			shapes = cmds.listRelatives(obj, shapes=True)
-			for itm in shapes:
-				shapenodes.append(itm)
-		else:
-			shapenodes.append(obj)
-
-	for shape in shapenodes:
-		# Check if GuerillaTags attribute exists on selected
-		has_guerilla_tags = cmds.attributeQuery('GuerillaTags', node=shape, exists=True)
-		# If attr GuerillaTags doesnt exists, create it
-		if not has_guerilla_tags:
-			print('attribute already exists')
-		else:
-			# Delete an attributes.
-			cmds.deleteAttr('{}.GuerillaTags'.format(shape))
-
-def selectObjectsNoTags():
-
-	objects = cmds.ls(geometry=True)
-	not_tagged = []
-
-	for obj in objects:
-	    is_tagged = False
-
-	    has_guerilla_tags = cmds.attributeQuery('GuerillaTags',
-	                                             node=obj,
-	                                             exists=True)
-
-	    if has_guerilla_tags:
-	        existing_tags = cmds.getAttr('{}.GuerillaTags'.format(obj))
-	        existing_tags = existing_tags.replace(' ', '')
-
-	        if existing_tags != '':
-	            is_tagged = True
-
-	    if not is_tagged:
-	        not_tagged.append(obj)
-	        cmds.select(not_tagged)
-
-def makeWindow():
-	if (cmds.window("Pipeline", exists=True)):
-			cmds.deleteUI("Pipeline")
-	#def Pipeline():
-	window = cmds.window("Guerilla Tags", title="Guerilla Tags", iconName='Guerilla Tags', w=50,h=150,sizeable=False)
-
-	colonneP=cmds.rowColumnLayout(adjustableColumn=True)
-	cmds.iconTextButton( style="iconAndTextHorizontal", image=Imagepath+"icon-GuerillaTags_02.png",h=80,al="center", backgroundColor=[0.15, 0.15, 0.15])
-	cmds.text( label='',h=5 )
-
-	#---------- TAG MANAGER ----------
-	cmds.columnLayout('GuerillaTagsLayout', adjustableColumn=True,h=200)
-	cmds.optionMenu('prefix_tags',h=30)
-	cmds.menuItem( label='Prefix - None' )
-
-#-------------------------------------------- CHANGE, REMOVE OR ADD PREFIXE -------------------------------------------------
-
-	cmds.menuItem( label='Student' )
-	cmds.menuItem( label='Granny' )
-	cmds.menuItem( label='Schoolgirl' )
-	cmds.menuItem( label='WorkingWoman' )
-	cmds.menuItem( label='Cashier' )
-	cmds.menuItem( label='Kitchen' )
-	cmds.menuItem( label='Bridge' )
-	cmds.menuItem( label='Supermarket' )
-	cmds.menuItem( label='Elevator' )
-	cmds.menuItem( label='Classroom' )
+def _iter_geo_with_tags():
+    for geo in cmds.ls(geometry=True):
+        if cmds.attributeQuery(
+                'GuerillaTags',
+                node=geo,
+                exists=True):
+            yield geo
 
 
-	cmds.textFieldGrp('guerilla_tags',pht='Tags...',adj=1,h=30)
-	cmds.text( label='',h=5)
-	cmds.button( label='Add Tags Selection', command="SetTags()")
-	cmds.button( label='Delete Tags Selection', command="DeleteTags()" )
-	cmds.button( label='Select Objects not Tags', command="selectObjectsNoTags()" )
-	cmds.setParent('..')
-	
+def _get_tags(obj):
+    try:
+        raw_tags = cmds.getAttr('{}.GuerillaTags'.format(obj))
+    except ValueError:  # Raised when attribute does not exist
+        raw_tags = ""
+    return set(TAG_PATTERN.findall(raw_tags))
 
-	cmds.showWindow( window )
 
-makeWindow()
+def _set_tags(obj, tags):
+    if not cmds.attributeQuery("GuerillaTags", node=obj, exists=True):
+        cmds.addAttr(obj, longName="GuerillaTags", dataType="string")
+    cmds.setAttr('{}.GuerillaTags'.format(obj),
+                 ', '.join(set(tags)),
+                 type='string')
 
-#END
+
+def _shapes_under_selection():
+    selected = cmds.ls(
+        selection=True, type="shape") or []
+    relatives = cmds.listRelatives(
+        allDescendents=True, type="shape", path=True) or []
+    return selected + relatives
+
+
+class GuerillaTagsWindow(object):
+    """
+    Class containing both logic and UI for the guerilla tags tool.
+    """
+
+    def __init__(self):
+        print("zab")
+        if (cmds.window("GuerillaTagsWindow", exists=True)):
+                cmds.deleteUI("GuerillaTagsWindow")
+
+        window = cmds.window(
+            "GuerillaTagsWindow",
+            title="Guerilla Tags",
+            iconName='Guerilla Tags',
+            w=50, h=150, sizeable=False)
+
+        cmds.rowColumnLayout(adjustableColumn=True)
+        cmds.iconTextButton(
+            style="iconAndTextHorizontal",
+            image=IMAGE_PATH + "icon-GuerillaTags_02.png",
+            h=80, al="center", backgroundColor=[0.15, 0.15, 0.15])
+        cmds.text(label='', h=5)
+
+        cmds.columnLayout('GuerillaTagsLayout', adjustableColumn=True, h=200)
+
+    #-------------------------------------------- CHANGE, REMOVE OR ADD PREFIXE -------------------------------------------------
+
+        cmds.textFieldGrp('guerilla_tags', pht='Tags...', adj=1, h=30)
+        cmds.text(label='', h=5)
+        cmds.button(label='Insert Tag(s) to selection', command=self.insert_tags)
+        cmds.button(label='Replace Tag(s) on selection', command=self.replace_tags)
+        cmds.button(label='Remove Tag(s) from Selection', command=self.remove_tags)
+        cmds.button(label='Select matching Tag(s)', command=self.select_tags)
+
+        cmds.text(label='', h=5)
+        cmds.button(label='Clear Tags on selection', command=self.clear_tags)
+        cmds.button(label='Select Objects not Tags', command=self.select_no_tags)
+        cmds.setParent('..')
+
+        print("zob")
+        cmds.showWindow(window)
+
+    def get_user_tags(self):
+        raw = cmds.textFieldGrp('guerilla_tags', tx=1, q=1)
+        return set(TAG_PATTERN.findall(raw))
+
+    def insert_tags(self, *args):
+        new_tags = self.get_user_tags()
+        for shape in _shapes_under_selection():
+            old_tags = _get_tags(shape)
+            _set_tags(shape, new_tags | old_tags)
+
+    def replace_tags(self, *args):
+        new_tags = self.get_user_tags()
+        for shape in _shapes_under_selection():
+            _set_tags(shape, new_tags)
+
+    def remove_tags(self, *args):
+        new_tags = self.get_user_tags()
+        for shape in _shapes_under_selection():
+            old_tags = _get_tags(shape)
+            _set_tags(shape, old_tags - new_tags)
+
+    def select_tags(self, *args):
+        user_tags = self.get_user_tags()
+        cmds.select(clear=True)
+        for shape in _iter_geo_with_tags():
+            # Test if user tags are included in existing tags on shape
+            if user_tags <= _get_tags(shape):
+                cmds.select(shape, add=True)
+
+    def clear_tags(self, *args):
+        for shape in _shapes_under_selection():
+            _set_tags(shape, [])
+
+    def select_no_tags(self, *args):
+        cmds.select(clear=True)
+        for obj in _iter_geo_with_tags():
+            if not _get_tags(obj):
+                cmds.select(obj, add=True)
+
+if __name__ == "__main__":
+    GuerillaTagsWindow()
